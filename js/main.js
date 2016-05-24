1f7e2cdc455e4442721ea10a03a447665cdee191
lenadis <lenadisr@gmail.com>
2016-05-24 22:27:40 +0200

    communicates with api, gets random movie

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
+var myApp = angular.module('myApp',['ui.router']);
+
+myApp.controller('MyController', function MyController($scope, $http) {     console.log("Good day sir")
+    $http.get("api.json")
+//    $http.get("http://nodeexamapi.herokuapp.com/movies")
+    .then(function(response) {
+        $scope.allmovies = response.data;
+        myrandnumb = Math.floor(Math.random() * $scope.allmovies.length);
+        $scope.movie = response.data[myrandnumb];                       
+        console.log($scope.movie);
+    });
+    
+});
+
+myApp.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/index.html");
+  
+  $stateProvider
+//        .state('frontpage', {
+//          url: "/frontpage",
+//          templateUrl: "partials/frontpage.html"
+//        })
+    
+      .state('movie', {
+      url: "/movie",
+      templateUrl: "partials/movie.html"
+    })
+    
+    
+});
+
 

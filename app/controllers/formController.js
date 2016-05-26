bc7fbe67d5ea8d9eef34b27675511e61b91f6cbd
Teresa Sorbera <TeresaSorbera.traductrice@gmail.com>
2016-05-26 18:05:57 +0200

    submitbutton

--- app/controllers/formController.js
+++ app/controllers/formController.js
@@ -1,5 +1,49 @@
 var movieApp = angular.module("movieApp");
 
 movieApp.controller('allMoviesController', function ($scope, $state, $stateParams, $http) {
+    console.log("hi");
+    $scope.contact = {};
+
+    $scope.saveForm = function () {
+    console.log($scope.contact);
+        $http({
+            method: "POST",
+            data: $scope.contact,
+            url: "http://nodeexamapi.herokuapp.com/movies"
+        })
+            .success(function (data) {
+                //deferred.resolve(data);
+
+            }).error(function (data) {
+            //deferred.reject(data);
+
+        });
+
+    };
+
+        var self = this;
+        self.submit = function() {
+            console.log('User clicked submit with ', self.user);
+        };
+
+        $scope.list = [];
+        $scope.text = 'hello';
+        $scope.submit = function() {
+            if ($scope.text) {
+                $scope.list.push(this.text);
+                $scope.text = '';
+            }
+        };
+
+        $scope.contacts = ["hi@me.no", "hi@you.com"];
+
+        $scope.addMail = function(mailAddress) {
+            if($scope.mailAddress) {
+                $scope.contacts.push(mailAddress);
+                $scope.mailAddress = "";
+            }
+        };
+
+
 
 });

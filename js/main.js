182acacf5e971306b8a8998ee75bb9b450d7ecff
lenadis <lenadisr@gmail.com>
2016-05-27 18:02:52 +0200

    Filters and Random work

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
 var myApp = angular.module('myApp',['ui.router']);
 
-myApp.controller('MyController', function MyController($scope, $http) {     console.log("Good day sir")
+myApp.controller('MyController', function MyController($scope, $http) {
     $http.get("api.json")
 //    $http.get("http://nodeexamapi.herokuapp.com/movies")
     .then(function(response) {
         $scope.allmovies = response.data;
         myrandnumb = Math.floor(Math.random() * $scope.allmovies.length);
-        $scope.movie = response.data[myrandnumb];                       
-        console.log($scope.movie);
+        $scope.movie = response.data[myrandnumb];
     });
-    
+    $scope.randomMovie
+    $scope.filterByGenre = function(genre) {
+        filteredarray = []
+        for(i=0; i < $scope.allmovies.length; i++){
+            if ($scope.allmovies[i].genre == genre){
+                filteredarray.push($scope.allmovies[i])
+            }     
+        }
+        myrandnumb = Math.floor(Math.random() * filteredarray.length);
+        $scope.movie = filteredarray[myrandnumb];
+    };
+                                                                       
+                                                                       
 });
 
 myApp.config(function($stateProvider, $urlRouterProvider) {
